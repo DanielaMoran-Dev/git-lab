70570ff6e3ce6f7d2f10ebe2112cbbbd3871ed10
Daniela Moran <141843938+DanielaMoran-Dev@users.noreply.github.com>
2026-09-22 07:46:22 -0600

    feat (parcial02) - remove primary constructor

--- src/EventsHub.Api/Controllers/EventsHubBaseController.cs
+++ src/EventsHub.Api/Controllers/EventsHubBaseController.cs
@@ -1,9 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using MediatR;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 
 namespace EventsHub.Api.Controllers;
 
 [Route("api/v1/[controller]")]
 [ApiController]
-public abstract class EventsHubBaseController : ControllerBase
+public class EventsHubBaseController : ControllerBase
 {
+    private IMediator? _mediator;
+
+    protected IMediator Mediator => 
+        _mediator ??= HttpContext.RequestServices.GetService<Mediator>()
+            ?? throw new InvalidOperationException("IMediator service in unavailable");
 }
